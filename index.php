53f97098a2652e8df4e7a4689304d67e9c656ba8
DESKTOP-62NBI5U\yshkw <yshkwi.chacha@gmail.com>
2023-03-14 03:45:32 +0900

    投稿ループ文完成

--- index.php
+++ index.php
@@ -1,61 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- ヘッターの読み込み -->
+<?php get_header(); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/reset.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
-    <title>chatan</title>
-</head>
 
 <body>
     <main>
         <h2>Latest Articles</h2>
+
+     <!-- WP投稿ページ -->
+     <?php
+        if (have_posts()) :
+        while (have_posts()) :
+        the_post();
+      ?>
+
         <div class="articles-wrap">
             <div class="articles">
                 <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
-                <span>2018/5/20</span>
-                <p>おしゃれカフェがありますよ</p>
+                <span><?php echo get_the_date(); ?></span>
+                <p><?php the_title(); ?></p>
                 <a href="">READ MORE</a><!--リードの棒びゆーんさせる- -->
             </div>
-            <div class="articles">
-                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
-                <span>2018/5/20</span>
-                <p>おしゃれカフェがありますよ</p>
-                <a href="">READ MORE</a>
-            </div>
-            <div class="articles">
-                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
-                <span>2018/5/20</span>
-                <p>おしゃれカフェがありますよ</p>
-                <a href="">READ MORE</a>
-            </div>
-            <div class="articles">
-                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
-                <span>2018/5/20</span>
-                <p>おしゃれカフェがありますよ</p>
-                <a href="">READ MORE</a>
-            </div>
-            <div class="articles">
-                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
-                <span>2018/5/20</span>
-                <p>おしゃれカフェがありますよ</p>
-                <a href="">READ MORE</a>
-            </div>
-            <div class="articles">
-                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
-                <span>2018/5/20</span>
-                <p>おしゃれカフェがありますよ</p>
-                <a href="">READ MORE</a>
-            </div>
-
         </div>
-    </main>
-    <footer>
-        <span>Copyrigth <small>&copy;</small>Shinohara.ALL RIGHTS RESERVED.</span>
-    </footer>
-</body>
+     <?php //メインループの記載ワイル分のおわり
+         endwhile;
+         else : //メインループの記載 if文のつづき
+      ?>
+      <div class="articles-wrap" >
+      <h2 class="subtitle">表示する記事がありません</h2>
+      </div>
+     <?php endif;//メインループの記載 if文の終わり ?>
+    <!-- メインループここまで -->
 
-</html>
+    </main>
+<!-- フッターの読み込み -->
+<?php get_footer(); ?>

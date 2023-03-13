143d0cf8ced63923969e055c3e5e3712eaa72d22
DESKTOP-62NBI5U\yshkw <yshkwi.chacha@gmail.com>
2023-03-14 03:09:20 +0900

    ワードプレス　リンク切れ解消

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/reset.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/assets/css/style.css">
+    <title>chatan</title>
+</head>
+
+<body>
+    <main>
+        <h2>Latest Articles</h2>
+        <div class="articles-wrap">
+            <div class="articles">
+                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
+                <span>2018/5/20</span>
+                <p>おしゃれカフェがありますよ</p>
+                <a href="">READ MORE</a><!--リードの棒びゆーんさせる- -->
+            </div>
+            <div class="articles">
+                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
+                <span>2018/5/20</span>
+                <p>おしゃれカフェがありますよ</p>
+                <a href="">READ MORE</a>
+            </div>
+            <div class="articles">
+                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
+                <span>2018/5/20</span>
+                <p>おしゃれカフェがありますよ</p>
+                <a href="">READ MORE</a>
+            </div>
+            <div class="articles">
+                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
+                <span>2018/5/20</span>
+                <p>おしゃれカフェがありますよ</p>
+                <a href="">READ MORE</a>
+            </div>
+            <div class="articles">
+                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
+                <span>2018/5/20</span>
+                <p>おしゃれカフェがありますよ</p>
+                <a href="">READ MORE</a>
+            </div>
+            <div class="articles">
+                <img src="<?php echo get_template_directory_uri();?>/folder/post_img_1.png" alt="北谷画像">
+                <span>2018/5/20</span>
+                <p>おしゃれカフェがありますよ</p>
+                <a href="">READ MORE</a>
+            </div>
+
+        </div>
+    </main>
+    <footer>
+        <span>Copyrigth <small>&copy;</small>Shinohara.ALL RIGHTS RESERVED.</span>
+    </footer>
+</body>
+
+</html>
